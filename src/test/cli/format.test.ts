b3377a0cb8980daf5739577ea352870a0f46f152
Zhao DAI <zddai@amazon.com>
2021-01-29 17:08:02 +0000

    Fix dir compare bug.

--- src/test/cli/format.test.ts
+++ src/test/cli/format.test.ts
@@ -4,14 +4,15 @@ import {
 } from 'child_process';
 import { compareSync } from 'dir-compare';
 import fs from 'fs-extra';
-import path from 'path';
+import path, { sep } from 'path';
 import tmp from 'tmp';
 
 import { assertNonNull } from '../../lib/common';
 
 const CMD = 'cmd.txt';
 const STDIN = 'stdin.txt';
-const BASE_DIR = '__base';
+const IN_DIR = '__in';
+const OUT_DIR = '__out';
 
 describe('cli/format-imports', () => {
   test('--help', () => run('--help'));
@@ -27,33 +28,17 @@ describe('cli/format-imports', () => {
   //  runTestSuite(examples, 'eslint/sort');
 });
 
-function getCmd(dir: string) {
-  return readFile(dir, CMD)?.split('\n');
-}
-
-function getStdin(dir: string) {
-  const out = path.resolve(dir, STDIN);
-  return fs.existsSync(out) && fs.statSync(out).isFile() ? out : undefined;
-}
-
-function getBaseDir(dir: string) {
-  const out = path.resolve(dir, BASE_DIR);
-  return fs.existsSync(out) && fs.statSync(out).isDirectory() ? out : undefined;
-}
-
-function readFile(dir: string, fn: string) {
-  const file = path.resolve(dir, fn);
-  if (!fs.existsSync(file)) return undefined;
-  return fs.readFileSync(file).toString();
-}
-
 function runTestSuite(resolved: string, relative?: string | string[]): void {
-  const [name, ...rest] = relative ? (typeof relative === 'string' ? [relative] : relative) : [];
+  const [name, ...rest] = relative
+    ? typeof relative === 'string'
+      ? relative.split(sep)
+      : relative
+    : [];
   if (name) return describe(name, () => runTestSuite(path.resolve(resolved, name), rest));
   const entries = fs.readdirSync(resolved, { withFileTypes: true });
   if (entries.some(e => e.name === CMD)) runTestCase(resolved);
   entries
-    .filter(e => e.isDirectory() && e.name !== BASE_DIR)
+    .filter(e => e.isDirectory() && e.name !== IN_DIR && e.name != OUT_DIR)
     .forEach(e => describe(e.name, () => runTestSuite(path.resolve(resolved, e.name))));
 }
 
@@ -65,18 +50,21 @@ function runTestCase(resolved: string) {
 
 function runCmd(options: string, resolved: string) {
   const stdin = getStdin(resolved);
-  // create a tmp directory as the base directory to sandbox child process.
-  const tmpDir = tmp.dirSync();
-  const baseDir = tmpDir.name;
-  // copy files needed to sandbox base directory.
-  const baseSrc = getBaseDir(resolved);
-  if (baseSrc) fs.copySync(baseSrc, baseDir);
+  // create a tmp directory as the base directory to sandbox the child process.
+  const tmpDir1 = tmp.dirSync();
+  const baseDir = tmpDir1.name;
+  // copy files needed to the child base directory.
+  const { inDir, outDir } = getDirs(resolved);
+  if (inDir) fs.copySync(inDir, baseDir);
   run(options, { stdin, baseDir });
-  // check base directory content, or ensure there is no files inside.
-  if (baseSrc) {
-    const r = compareSync(baseSrc, baseDir, { compareContent: true });
-    expect(r.same).toBeTruthy();
-  } else expect(fs.readdirSync(baseDir).length).toBe(0);
+  // setup the expected base directory
+  const tmpDir2 = tmp.dirSync();
+  const baseDirExpected = tmpDir2.name;
+  if (inDir) fs.copySync(inDir, baseDirExpected);
+  if (outDir) fs.copySync(outDir, baseDirExpected);
+  // check base directory content.
+  const r = compareSync(baseDirExpected, baseDir, { compareContent: true });
+  expect(r.same).toBeTruthy();
 }
 
 function run(options: string, env?: { stdin?: string; baseDir: string }) {
@@ -94,4 +82,30 @@ function run(options: string, env?: { stdin?: string; baseDir: string }) {
     stderr: stderr.toString(),
     status,
   }).toMatchSnapshot();
+}
+
+function getCmd(dir: string) {
+  return readFile(dir, CMD)?.split('\n');
+}
+
+function getStdin(dir: string) {
+  const out = path.resolve(dir, STDIN);
+  return fs.existsSync(out) && fs.statSync(out).isFile() ? out : undefined;
+}
+
+function getDirs(dir: string) {
+  const inDir = getDir(dir, IN_DIR);
+  const outDir = getDir(dir, OUT_DIR);
+  return { inDir, outDir };
+}
+
+function getDir(dir: string, name: string) {
+  const r = path.resolve(dir, name);
+  return fs.existsSync(r) && fs.statSync(r).isDirectory() ? r : undefined;
+}
+
+function readFile(dir: string, fn: string) {
+  const file = path.resolve(dir, fn);
+  if (!fs.existsSync(file)) return undefined;
+  return fs.readFileSync(file).toString();
 }
