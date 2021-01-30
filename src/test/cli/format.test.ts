f400d724614ba2b1d0e2a45194511f83371965be
Zhao DAI <zddai@amazon.com>
2021-01-30 14:33:11 +0000

    Add tests.

--- src/test/cli/format.test.ts
+++ src/test/cli/format.test.ts
@@ -62,12 +62,12 @@ function runCmd(options: string, resolved: string) {
 function run(options: string, env?: { stdin?: string; baseDir: string }) {
   // setup args
   const script = path.resolve('src/bin/format-cli.ts');
-  const args = ['-T', script, ...options.split(' ')].filter(a => !!a);
+  const args = [script, ...options.split(' ')].filter(a => !!a);
   // setup CWD and STDIN for child process if needed.
   const cwd = env?.baseDir;
   const stdio = env?.stdin ? [fs.openSync(env.stdin, 'r')] : undefined;
   const opt: SpawnSyncOptions = { cwd, stdio };
-  const { stdout, stderr, status } = spawnSync('ts-node-script', args, opt);
+  const { stdout, stderr, status } = spawnSync('ts-node-script', ['-T', ...args], opt);
   // check execution results
   expect({
     stdout: stdout.toString(),
