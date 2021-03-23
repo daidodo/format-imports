772dcbd4c6f13843866a86c4940233a8deffee8a
Zhao DAI <zddai@amazon.com>
2021-03-23 23:49:15 +0000

    Add cache to improve tsconfig loading performance.

--- src/lib/format/config/tsconfig.ts
+++ src/lib/format/config/tsconfig.ts
@@ -1,7 +1,10 @@
+import NodeCache from 'node-cache';
 import ts, { sys } from 'typescript';
 
 import { logger } from '../../common';
 import { parentFolder } from '../../config/helper';
+
+const CACHE = new NodeCache({ stdTTL: 5 });
 
 export function loadTsConfig(fileName: string, configPath?: string) {
   const log = logger('format-imports.loadTsConfig');
@@ -9,11 +12,14 @@ export function loadTsConfig(fileName: string, configPath?: string) {
     log.debug('Finding TS config for fileName:', fileName);
     const configFile = configPath || ts.findConfigFile(fileName, sys.fileExists.bind(sys));
     if (!configFile) return undefined;
+    const opt = CACHE.get(configFile);
+    if (opt && typeof opt === 'object') return opt as ts.CompilerOptions;
     log.debug('Loading TS config from:', configFile);
     const { config } = ts.readConfigFile(configFile, sys.readFile.bind(sys));
     const path = parentFolder(configFile);
     log.debug('Parsing TS config for path:', path);
     const { options } = ts.parseJsonConfigFileContent(config, sys, path);
+    CACHE.set(configFile, options);
     return options;
   } catch (e: unknown) {
     const msg = e instanceof Error ? e.message : `${e}`;
