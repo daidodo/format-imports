ac511b37f154cde44a02e5a6a2ffe13150153423
Zhao DAI <zddai@amazon.com>
2021-12-19 22:37:04 +0000

    Fix eslint warning.

--- src/bin/options.ts
+++ src/bin/options.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 const { name: EXE, version: VERSION } = require('../../package.json');
 
 const optionator: (options: any) => Results = require('optionator');
