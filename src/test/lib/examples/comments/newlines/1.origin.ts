da55db6a9bc2d37b39ed0a5b7d268c9955681380
Zhao DAI <zddai@amazon.com>
2022-02-13 14:30:25 +0000

    Update API docs.

---
+++ src/test/lib/examples/comments/newlines/1.origin.ts
@@ -0,0 +1,10 @@
+import A from 'a'
+
+// comment 1
+
+import B from 'b'
+
+
+// comment 2
+
+export {A, B}
---
+++ src/test/lib/examples/comments/newlines/2.origin.ts
@@ -0,0 +1,12 @@
+import A from 'a'
+
+// comment 1
+
+
+import B from 'b'
+
+
+// comment 2
+
+
+export {A, B}
