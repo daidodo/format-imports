e8a9776b35075cd9bb374ba3a4e3d1114f4bcfdc
daidodo <daidodo@gmail.com>
2022-04-09 00:07:35 +0100

    Add version.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 export * from './config';
 
 export * from './format/main';
+
+/**
+ * API version.
+ */
+export const { version } = require('../../package.json');
