c688ebca5e93db67e2744963684bfcf565e16160
Karl Horky <karl.horky@gmail.com>
2022-10-28 18:44:07 +0200

    Add tests for use client

---
+++ src/test/lib/examples/shebang/use/all/1.result.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+'use strict'
+'use client'
+import 'b';
+
+import A from 'a';
+import { B } from 'c';
+
+export { A, B }
---
+++ src/test/lib/examples/shebang/use/client/2.result.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+'use client'
+
+import 'b';
+
+import A from 'a';
+import { B } from 'c';
+
+export { A, B }
