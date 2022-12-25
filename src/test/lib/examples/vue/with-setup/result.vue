8b6878c8c7e986c21bf7ca4cfaee1cf598b282d8
changfeng <changfeng@gaoding.com>
2022-12-25 22:59:03 +0800

    test: add some tests for vue

---
+++ src/test/lib/examples/vue/with-setup/result.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>box</div>
+</template>
+
+<script setup lang="ts">
+import {
+  A,
+  B,
+} from 'a';
+import { E } from 'b';
+
+export { A, B, E };
+</script>
+
+<style lang="less">
+
+.a {
+    .b
+}
+</style>
