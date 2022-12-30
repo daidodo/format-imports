0095a921ffc797b7e43a0b94bfdf5dc24b8fc949
Zhao DAI <zddai@amazon.com>
2022-12-30 23:31:32 +0000

    minor.

---
+++ src/test/lib/examples/vue/using-component/dynamic.result.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Bar from './Bar.vue'
+import Foo from './Foo.vue'
+</script>
+
+<template>
+  <component :is="Foo" />
+  <component :is="someCondition ? Foo : Bar" />
+</template>
