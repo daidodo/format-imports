c1923624425e1f1f48400bb3ff331481897f7f76
Zhao DAI <zddai@amazon.com>
2024-04-15 00:06:06 +0100

    add tests.

--- src/test/lib/examples/attributes/assert.origin.ts
+++ src/test/lib/examples/attributes/assert.origin.ts
@@ -1,7 +1,26 @@
 import A from 'a' assert {  }
 import D from 'a' assert { a: '' }
 import B from 'b' assert { ab:    'xyz' }
-import C from 'c' assert { a: 'x',bc:'xyz' }
+import C from 'c' assert { a: 'x',bc:'xyz', asdfads: "adg",
+gaefasd: "gege",
+gaeaesadsasdf: "agefasdfasdgasdf", }
 import E from 'b' with { ab:    'xyz' }
 
-export {A, B, C,D,E}
+import {debounce,
+  isEmpty,
+  omitBy,} from 'b' assert { ab:    'xyz' }
+
+
+export {A, B, C,D,E,debounce,
+  isEmpty,
+  omitBy, OnboardingStatusFilter,
+  PaymentsStatusFilter}
+
+  import {  OnboardingStatusFilter,
+    PaymentsStatusFilter,} from 'd' assert {
+    a: 'x',
+    bc: 'xyz',
+    asdfads: 'adg',
+    gaefasd: 'gege',
+    gaeaesadsasdf: 'agefasdfasdgasdf',
+  };
--- src/test/lib/examples/attributes/with.result.ts
+++ src/test/lib/examples/attributes/with.result.ts
@@ -1,7 +1,30 @@
 import A from 'a';
 import D from 'a' with { a: '' };
-import B from 'b' with { ab: 'xyz' };
-import C from 'c' with { a: 'x', bc: 'xyz' };
-import E from 'c' assert { a: 'x', bc: 'xyz' };
+import B, {
+  debounce,
+  isEmpty,
+  omitBy,
+} from 'b' with { ab: 'xyz' };
+import E from 'b' assert { ab: 'xyz' };
+import C from 'c' with {
+  a: 'x',
+  bc: 'xyz',
+  asdfads: 'adg',
+  gaefasd: 'gege',
+  gaeaesadsasdf: 'agefasdfasdgasdf',
+};
+import {
+  OnboardingStatusFilter,
+  PaymentsStatusFilter,
+} from 'c' with {
+  a: 'x',
+  bc: 'xyz',
+  asdfads: 'adg',
+  gaefasd: 'gege',
+  gaeaesadsasdf: 'agefasdfasdgasdf1',
+};
 
-export {A, B, C,D,E}
+export {A, B, C,D,E,debounce,
+  isEmpty,
+  omitBy, OnboardingStatusFilter,
+  PaymentsStatusFilter}
